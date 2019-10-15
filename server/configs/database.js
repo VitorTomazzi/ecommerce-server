158d29c638dfd3af64d7c351f223a8c13f20eff1
Vitor Tomazzi <vitor.c.tomazzi@gmail.com>
2019-10-15 18:06:09 -0400

    signup

--- server/configs/database.js
+++ server/configs/database.js
@@ -1,15 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
+console.log(process.env);
 // Don't forget to set "MONGODB_URI" in ~/server/.env
-const uri =
-  process.env.MONGODB_URI ||
-  `mongodb://localhost/please-set-process-env-mongodb-uri`
+const uri = process.env.MONGODB_URI || `mongodb://localhost/cerealbars`;
 
 mongoose
-  .connect(uri, { useNewUrlParser: true })
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  })
+	.connect(uri, { useNewUrlParser: true })
+	.then((x) => {
+		console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+	})
+	.catch((err) => {
+		console.error('Error connecting to mongo', err);
+	});
